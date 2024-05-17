aa1b759404483f3b77fb6da18e9aefd0f43e0221
konaten <konatenhamed@gmail.com>
2024-05-17 00:18:27 +0000

    appplication fixtures

--- src/DataFixtures/EntrepriseFixtures.php
+++ src/DataFixtures/EntrepriseFixtures.php
@@ -13,18 +13,9 @@ class EntrepriseFixtures extends Fixture
     {
         $entreprise = new Entreprise();
         $entreprise->setDenomination('Default');
-        $entreprise->setSigle('ENT1');
-        $entreprise->setAdresse('ENT1');
-        $entreprise->setAgrements('ENT1');
-        $entreprise->setEmail('ENT1');
-        $entreprise->setContacts('ENT1');
-        $entreprise->setFax('ENT1');
-        $entreprise->setSituationGeo('ENT1');
-        $entreprise->setSiteWeb('ENT1');
         $entreprise->setCode('ENT1');
-        $entreprise->setMobile('ENT1');
-       // $entreprise->setLogo('ENT1');
-       // $entreprise->set('ENT1');
+        // $entreprise->setLogo('ENT1');
+        // $entreprise->set('ENT1');
         // $product = new Product();
         $manager->persist($entreprise);
 
